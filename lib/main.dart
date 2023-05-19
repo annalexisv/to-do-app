24b88c7a0517b5abcfc14aec9ca22b65e5741a7f
annalexisv <annalexisv@gmail.com>
2023-05-19 14:25:44 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/pages/home_page.dart';
+void main() {
+  runApp(ToDoApp());
+}
+
+class ToDoApp extends StatelessWidget {
+  const ToDoApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+      theme: ThemeData(primarySwatch: Colors.yellow),
+    );
+  }
+}
+
